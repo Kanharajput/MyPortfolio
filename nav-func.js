6d982abfcb4c9023ea4ad7811c4e8bed3a4105ad
Kanha Tomar <kanharajput91@gmail.com>
2023-06-24 20:26:59 +0530

    navbar fixed with javascript

--- nav-func.js
+++ nav-func.js
@@ -1,7 +1,9 @@
 // setup nav
 const navBtn = document.getElementById("nav-btn");
+// for small screen menu
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+const nav = document.getElementById("nav-container")
 // show nav
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
@@ -10,7 +12,12 @@ navBtn.addEventListener("click", () => {
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
 });
-//setup date
-const date = (document.getElementById(
-  "date"
-).innerHTML = new Date().getFullYear());
+
+// add fixed id to navbar, it only added when user slides the page
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 80) {
+    nav.setAttribute('id','navbar-fixed')
+  } else {
+    nav.removeAttribute('id','navbar-fixed')
+  }
+});
